d3c9b7fc213be1a14a2cbde0b55ca752e5b41cf8
szysza26 <szy162@gmail.com>
2024-10-27 18:44:21 +0100

    initialize frontend - react and material ui

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import './App.css'
+import { CssBaseline } from '@mui/material';
+
+function App() {
+  return (
+    <>
+      <CssBaseline/>
+      <h4>Hello world!</h4>
+    </>
+  )
+}
+
+export default App
